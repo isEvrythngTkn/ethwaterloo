4af433194c8b2182d6580d6d68c79d42c284b123
Paul Goertzen <paulngoertzen@gmail.com>
2017-10-15 01:51:59 -0400

    back to norms

--- src/containers/ViewExpenses.js
+++ src/containers/ViewExpenses.js
@@ -19,8 +19,8 @@ class ViewExpenses extends Component {
       name: '',
       description: '',
       limit: 0,
-      spenders: '',
-      funders: '',
+      spender: '',
+      funder: '',
       balance: 0,
       transactions: []
     }
@@ -36,39 +36,47 @@ class ViewExpenses extends Component {
       const contract = require('truffle-contract')
       const expensesContract = contract(ExpensesContract)
       expensesContract.setProvider(this.state.web3.currentProvider)
-      //var expenseContractInstance;
+      var expenseContractInstance;
 
       this.state.web3.eth.getAccounts((error, accounts) => {
         console.log(accounts);
         expensesContract.at(this.props.match.params.expenseID).then(function(instance){
           //self.setState({expenseContract: instance});
           window.contractInstance = instance;
+          expenseContractInstance = instance;
           console.log('expense contract', instance)
-          return instance.getContractData();
-          //return instance.state.call();
+          return expenseContractInstance.getContractData();
+          //return instance.state.call(); 
         }).then(function(data){
-          console.log(data[3]);
+          console.log(data);
           self.setState({
             name: self.state.web3.utils.hexToString(data[0]),
             state: self.state.web3.utils.hexToString(data[1]),
-            description: self.state.web3.utils.hexToString(data[2]),
-            limit: data[3].toNumber(),
-            spenders: data[4],
-            funders: data[5]
+            description: self.state.web3.utils.hexToString(data[2])
+          });
+          return expenseContractInstance.limitInCents();
+        }).then(function(data) {
+          console.log(data);
+          self.setState({limit: data.toNumber()});
+          return expenseContractInstance.funders();
+        }).then(function(data){
+          self.setState({funder: data});
+          return expenseContractInstance.spenders();
+        }).then(function(data) {
+          self.setState({spender: data});
+          return expenseContractInstance.limitInCents();
+        });
+      });
+      expensesContract.at(this.props.match.params.expenseID).then(function(instance){
+        self.setState({expenseContract: instance})
+        return instance.transactionsCount();
+      }).then(function(transCount){
+        for(var i = 0; i < transCount.toNumber(); i++) {
+          self.state.expenseContract.getTransactionData(i)
+          .then((data) => {
+            self.setState({transactions: [{amount: data[0].toNumber(), description: self.state.web3.utils.hexToString(data[1])}, ...self.state.transactions]})
           })
-        });
-
-        expensesContract.at(this.props.match.params.expenseID).then(function(instance){
-          self.setState({expenseContract: instance})
-          return instance.transactionsCount();
-        }).then(function(transCount){
-          for(var i = 0; i < transCount.toNumber(); i++) {
-            self.state.expenseContract.getTransactionData(i)
-            .then((data) => {
-              self.setState({transactions: [{amount: data[0].toNumber(), description: self.state.web3.utils.hexToString(data[1])}, ...self.state.transactions]})
-            })
-          }
-        });
+        }
       });
     })
     .catch(() => {
@@ -80,7 +88,7 @@ class ViewExpenses extends Component {
     return (
       <div className="App">
         <nav className="navbar pure-menu pure-menu-horizontal">
-          <Link to="/" className="pure-menu-heading pure-menu-link">Expense Report #{this.props.match.params.expenseID}</Link>
+          <a href="#" className="pure-menu-heading pure-menu-link">Expense Report #{this.props.match.params.expenseID}</a>
         </nav>
 
         <main className="container">
@@ -93,8 +101,9 @@ class ViewExpenses extends Component {
                   State: {this.state.state}<br/>
                   Description: {this.state.description}<br/>
                   Limit: {this.state.limit}<br/>
-                  Spenders: {this.state.spenders}<br/>
-                  Funders: {this.state.funders}<br/>
+                  Spender: {this.state.spender}<br/> 
+                  Funders: {this.state.funder}<br/>
+                  <button className="fund-contract">Fund Contract</button>
                 </fieldset>
                 <ul>
                   {this.state.transactions.map((transaction, index) => {
