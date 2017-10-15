c88a9e1cbc4e95d07c386f2b96ed6b4c0f1fd55b
Brian Richter <brian.richter@beanstream.com>
2017-10-14 23:40:19 -0400

    transactions creating

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import ListExpenses from './containers/ListExpenses'
 import NewExpenses from './containers/NewExpenses'
 import ViewExpenses from './containers/ViewExpenses'
+import Transaction from './containers/Transaction'
 
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
@@ -11,8 +12,9 @@ ReactDOM.render(
   <BrowserRouter>
     <Switch>
       <Route exact path='/' component={ListExpenses}/>
-      <Route path='/new' component={NewExpenses}/>
-      <Route path='/view/:expenseID' component={ViewExpenses}/>
+      <Route exact path='/new' component={NewExpenses}/>
+      <Route exact path='/view/:expenseID' component={ViewExpenses}/>
+      <Route path='/view/:expenseID/new-transaction' component={Transaction}/>
     </Switch>
   </BrowserRouter>,
   document.getElementById('root')
